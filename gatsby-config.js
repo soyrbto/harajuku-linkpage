1d569454acb1b63f488f630242fd6b516317a052
soyrbto <soyrbto@gmail.com>
2022-10-09 20:48:59 -0400

    adding analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,20 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-8KK1WJZV2T",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 }
